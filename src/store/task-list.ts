440e0f2fe912a976fcee3b7b18876a29364d1e61
Artem Boev <boev.dev@mail.ru>
2024-08-22 23:06:08 +0500

    delete test array of objects in task list

--- src/store/task-list.ts
+++ src/store/task-list.ts
@@ -9,28 +9,7 @@ export interface TaskType {
 }
 
 class TaskList {
-  list: TaskType[] = [
-    {
-      id: '32fsdfsdfsdfsd',
-      title: 'Задача 1',
-      description: 'Описание 1',
-    },
-    {
-      id: '32fsdfsdfsdfsde42312',
-      title: 'Задача 2',
-      description: 'Описание 2',
-    },
-    {
-      id: '32fsdfsdfsdfsd111111',
-      title: 'Задача 3',
-      description: 'Описание 3',
-    },
-    {
-      id: '32fsdfsdfsdfsd222222',
-      title: 'Задача 4',
-      description: 'Описание 4',
-    },
-  ];
+  list: TaskType[] = [];
   showDescription?: string;
 
   constructor() {
